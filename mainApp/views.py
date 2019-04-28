f0fdca915cd905200b693717160cdeeda7d5de91
Daulet Bimaganbetov <bimaganbetov1995@gmail.com>
2019-04-28 18:59:08 +0600

    new project

--- mainApp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-	return render(request, 'mainApp/index.html')
-
-def location(request):
-	return render(request, 'mainApp/location.html')
-
