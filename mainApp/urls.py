5cc73bffa9afee2cdc8c05e1c092bd4d660cf730
Daulet Bimaganbetov <bimaganbetov1995@gmail.com>
2019-05-06 17:43:45 +0600

    new commit

--- mainApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import *
-
-urlpatterns = [
-	path('', posts_list, name='posts_list_url'),
-	path('post/<str:slug>/', post_detail, name='post_detail_url'),
-	path('tags/', tags_list, name='tags_list_url'),
-]
