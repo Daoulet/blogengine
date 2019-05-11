6a2cd8bbaa2b646333fc828a6600690e15675fb7
Daulet Bimaganbetov <bimaganbetov1995@gmail.com>
2019-05-12 00:10:16 +0600

    add navig-3

--- blog/views.py
+++ blog/views.py
@@ -13,11 +13,31 @@ from django.core.paginator import Paginator
 
 def  post_list(request):
 	posts = Post.objects.all()
-	paginator = Paginator(posts, 2)
+	paginator = Paginator(posts, 10)
+
 	page_number = request.GET.get('page', 1)
 	page = paginator.get_page(page_number)
 
-	return render(request, 'blog/index.html', context={'posts': page})
+	is_paginated = page.has_other_pages()
+
+	if page.has_previous():
+		prev_url = '?page={}'.format(page.previous_page_number())
+	else:
+		prev_url = ''
+
+	if page.has_next():
+		next_url = '?page={}'.format(page.next_page_number())
+	else:
+		next_url = ''
+
+	context = {
+		'page_object': page,
+		'is_paginated': is_paginated,
+		'next_url': next_url,
+		'prev_url': prev_url
+	}
+
+	return render(request, 'blog/index.html', context=context)
 
 class PostDetail(ObjectDetailMixin, View):
 	model = Post
